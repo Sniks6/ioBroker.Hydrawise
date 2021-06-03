719a06c14e958a7d6be39d34e2899a49817cf1be
Sniks6 <alexander.henz@bodyhey.com>
2021-06-03 19:43:54 +0200

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"hydrawise adapter settings": {
+		"en": "Adapter settings for hydrawise",
+		"de": "Adaptereinstellungen für hydrawise",
+		"ru": "Настройки адаптера для hydrawise",
+		"pt": "Configurações do adaptador para hydrawise",
+		"nl": "Adapterinstellingen voor hydrawise",
+		"fr": "Paramètres d'adaptateur pour hydrawise",
+		"it": "Impostazioni dell'adattatore per hydrawise",
+		"es": "Ajustes del adaptador para hydrawise",
+		"pl": "Ustawienia adaptera dla hydrawise",
+		"zh-cn": "hydrawise的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "option2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	}
+};
